86dc2be8ac2254c453d6043a8a9c05c34efcefee
prsG20 <m22cs057@iitj.ac.in>
2026-05-11 15:41:19 +0530

    Initial Commit

---
+++ ScreenAsk/ScreenAskApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenAskApp.swift
+//  ScreenAsk
+//
+//  Created by Prashant Gautam on 11/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ScreenAskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
